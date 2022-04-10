c8f7751b39c5420de59e5e6bc7c48f27ffdb00db
adrianobarbosa1 <adriano.barbosa100@gmail.com>
2022-04-10 17:26:19 -0300

    att

--- view/webpack.config.js
+++ view/webpack.config.js
@@ -1,27 +1,23 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: isDevelopment ? 'development': 'production',
-  devtool: isDevelopment ? 'eval-source-map': 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
-    static: {
-      directory: path.join(__dirname, "public")
-    },
+    contentBase: path.resolve(__dirname, 'public'),
     hot: true,
-    compress: true,
-    port: 3010,
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),
@@ -32,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -41,13 +37,13 @@ module.exports = {
               isDevelopment && require.resolve('react-refresh/babel')
             ].filter(Boolean)
           }
-        }
+        },
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       }
-    ]
+    ],
   }
-}
+};
